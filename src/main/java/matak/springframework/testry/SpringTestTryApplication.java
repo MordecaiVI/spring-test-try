e14cbc5293bd9c98e21df5b7ffb7c391b2312951
mordecaivi <mordecaivv@gmail.com>
2019-10-17 14:04:20 +0200

    Init

---
+++ src/main/java/matak/springframework/testry/SpringTestTryApplication.java
@@ -0,0 +1,13 @@
+package matak.springframework.testry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestTryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestTryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/matak/springframework/testry/SpringTestTryApplicationTests.java
@@ -0,0 +1,13 @@
+package matak.springframework.testry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestTryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
